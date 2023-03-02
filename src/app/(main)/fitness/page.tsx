2de9d76d97551a8ffba7b4c5df23901c19a69290
Michael Kanahele (MAIN) <Kanahelebusiness@gmail.com>
2023-03-01 16:27:55 -0800

    Setup done

--- src/app/(main)/fitness/page.tsx
+++ src/app/(main)/fitness/page.tsx
@@ -2,15 +2,25 @@
 
 import { useContext, useEffect } from "react";
 import { MainContext } from "../../../contexts/main/mainContext";
+import { useRouter } from "next/navigation";
 
 import style from "./page.module.css";
+import LoadingSpinner from "../../../components/(elements)/loadingSpinner/loadingSpinner.component";
 
 const FitnessPage = () => {
-  const { mainDispatch } = useContext(MainContext);
+  const { mainDispatch, mainState } = useContext(MainContext);
+  const router = useRouter();
 
   useEffect(() => {
     mainDispatch({ type: "SET_PAGE", payload: { page: "FITNESS" } });
+
+    if (!mainState.fitnessPLan && !mainState.isLoading)
+      router.push("/setup/fitness");
   }, []);
+
+  if (mainState.isLoading || !mainState.fitnessPLan) {
+    return <LoadingSpinner />;
+  }
 
   return <div>Fitness</div>;
 };
--- src/app/(main)/nutrition/page.tsx
+++ src/app/(main)/nutrition/page.tsx
@@ -2,15 +2,25 @@
 
 import { useContext, useEffect } from "react";
 import { MainContext } from "../../../contexts/main/mainContext";
+import LoadingSpinner from "../../../components/(elements)/loadingSpinner/loadingSpinner.component";
+import { useRouter } from "next/navigation";
 
 import style from "./page.module.css";
 
 const NutritionPage = () => {
-  const { mainDispatch } = useContext(MainContext);
+  const { mainDispatch, mainState } = useContext(MainContext);
+  const router = useRouter();
 
   useEffect(() => {
     mainDispatch({ type: "SET_PAGE", payload: { page: "NUTRITION" } });
+
+    if (!mainState.nutritionPlan && !mainState.isLoading)
+      router.push("/setup/nutrition");
   }, []);
+
+  if (mainState.isLoading || !mainState.nutritionPlan) {
+    return <LoadingSpinner />;
+  }
 
   return <div>Nutrition</div>;
 };
